e79073801da8affb88c3d887f5f56b3c2dc9ac3a
Angela Smith <avksmit2@gmail.com>
2017-04-24 13:52:40 -0700

    first commit

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using Nancy;
+using MadLibs.Objects;
+
+namespace MadLibs
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
